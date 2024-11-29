193cd3bccb20651de44ba6ff96f29d234e555c02
Andrej Bachvarovski <96391874+Andrej03@users.noreply.github.com>
2024-11-28 19:44:37 -0500

    unit fixes

--- src/api/OpenWeatherData.js
+++ src/api/OpenWeatherData.js
@@ -2,7 +2,7 @@ const API_KEY = '57b4bcff1db5aae67ca96baa54d3ce60';
 const BASE_URL = 'https://api.openweathermap.org/data/2.5';
 
 export const getCurrentWeatherData = async (city = 'Toronto') => {
-    const url = `${BASE_URL}/weather?q=${city}&appid=${API_KEY}`;
+    const url = `${BASE_URL}/weather?q=${city}&appid=${API_KEY}&units=metric`;
 
     const response = await fetch(url);
     const data = await response.json();
@@ -14,7 +14,7 @@ export const getCurrentWeatherData = async (city = 'Toronto') => {
 };
 
 export const getWeeklyWeatherData = async (city = 'Toronto') => {
-    const url = `${BASE_URL}/forecast?q=${city}&appid=${API_KEY}`;
+    const url = `${BASE_URL}/forecast?q=${city}&appid=${API_KEY}&units=metric`;
 
     const response = await fetch(url);
     const data = await response.json();
